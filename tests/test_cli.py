755a6d2b7feb03032cea42ee9c87cdc8b9bc939a
Will Thompson <will.k.t@gmail.com>
2021-02-12 23:57:46 -0600

    completed cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,16 +23,40 @@ def count_lines(text):
     return len(matches)
 
 
-def test_script_on_form4():
-    assert app is not None
-    in_dir = Path(dir_path) / "data/form-4/sample/2020"
-    assert in_dir.exists()
+def test_script_on_form3_collection(test_form3_collection):
     with tempfile.TemporaryDirectory() as tmp_dir:
         out_dir = Path(tmp_dir)
         assert out_dir.exists()
         result = runner.invoke(
             app,
-            ["form4", str(in_dir), "--out_dir", str(tmp_dir)],
+            ["form4", str(test_form3_collection), "--out_dir", str(tmp_dir)],
+        )
+        assert result.exit_code == 0
+        assert "processing files in dir" in result.stdout
+        assert "generating output in dir" in result.stdout
+        assert result.stdout.count("processing file:") == 100
+
+        assert (Path(tmp_dir) / "document_info.csv").exists()
+        assert count_lines((Path(tmp_dir) / "document_info.csv").read_text()) == 100
+        assert (Path(tmp_dir) / "footnotes.csv").exists()
+        assert count_lines((Path(tmp_dir) / "footnotes.csv").read_text()) == 500
+        assert (Path(tmp_dir) / "derivatives.csv").exists()
+        assert count_lines((Path(tmp_dir) / "derivatives.csv").read_text()) == 128
+        assert (Path(tmp_dir) / "nonderivatives.csv").exists()
+        assert count_lines((Path(tmp_dir) / "nonderivatives.csv").read_text()) == 54
+        assert (Path(tmp_dir) / "report_owners.csv").exists()
+        assert count_lines((Path(tmp_dir) / "report_owners.csv").read_text()) == 178
+        assert (Path(tmp_dir) / "signatures.csv").exists()
+        assert count_lines((Path(tmp_dir) / "signatures.csv").read_text()) == 168
+
+
+def test_script_on_form4_collection(test_form4_collection):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        out_dir = Path(tmp_dir)
+        assert out_dir.exists()
+        result = runner.invoke(
+            app,
+            ["form4", str(test_form4_collection), "--out_dir", str(tmp_dir)],
         )
         assert result.exit_code == 0
         assert "processing files in dir" in result.stdout
@@ -51,3 +75,30 @@ def test_script_on_form4():
         assert count_lines((Path(tmp_dir) / "report_owners.csv").read_text()) == 129
         assert (Path(tmp_dir) / "signatures.csv").exists()
         assert count_lines((Path(tmp_dir) / "signatures.csv").read_text()) == 123
+
+
+def test_script_on_form5_collection(test_form5_collection):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        out_dir = Path(tmp_dir)
+        assert out_dir.exists()
+        result = runner.invoke(
+            app,
+            ["form4", str(test_form5_collection), "--out_dir", str(tmp_dir)],
+        )
+        assert result.exit_code == 0
+        assert "processing files in dir" in result.stdout
+        assert "generating output in dir" in result.stdout
+        assert result.stdout.count("processing file:") == 100
+
+        assert (Path(tmp_dir) / "document_info.csv").exists()
+        assert count_lines((Path(tmp_dir) / "document_info.csv").read_text()) == 100
+        assert (Path(tmp_dir) / "footnotes.csv").exists()
+        assert count_lines((Path(tmp_dir) / "footnotes.csv").read_text()) == 546
+        assert (Path(tmp_dir) / "derivatives.csv").exists()
+        assert count_lines((Path(tmp_dir) / "derivatives.csv").read_text()) == 138
+        assert (Path(tmp_dir) / "nonderivatives.csv").exists()
+        assert count_lines((Path(tmp_dir) / "nonderivatives.csv").read_text()) == 286
+        assert (Path(tmp_dir) / "report_owners.csv").exists()
+        assert count_lines((Path(tmp_dir) / "report_owners.csv").read_text()) == 102
+        assert (Path(tmp_dir) / "signatures.csv").exists()
+        assert count_lines((Path(tmp_dir) / "signatures.csv").read_text()) == 102
