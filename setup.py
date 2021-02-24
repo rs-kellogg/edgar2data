315f0ac25d36fbb559c3be5cf72aae9dcc707abc
Will Thompson <will.k.t@gmail.com>
2021-02-23 20:00:07 -0600

    update to setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="edgar",
+    name="edgar2data",
     version="0.2.0",
     author="Will Thompson",
     author_email="wkt@northwestern.edu",
     maintainer="Will Thompson",
     maintainer_email="wkt@northwestern.edu",
     description="Code for parsing EDGAR filings",
-    url="https://github.com/rs-kellogg/edgar",
+    url="https://github.com/rs-kellogg/edgar2data",
     packages=find_packages(include=["edgar", "edgar.*"]),
     package_data={"edgar": ["templates/*.j2"]},
     include_package_data=True,
